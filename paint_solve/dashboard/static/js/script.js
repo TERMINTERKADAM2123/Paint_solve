a512964928d6f6116e3ee7ca2aacfdcc4ea8335d
pravin kadam <pravinkadambiz@gmail.com>
2023-12-21 12:06:07 +0530

    forntend 40% done

--- paint_solve/dashboard/static/js/script.js
+++ paint_solve/dashboard/static/js/script.js
@@ -2,3 +2,62 @@
 function toggleTheme() {
     document.body.classList.toggle('dark-theme');
 }
+document.addEventListener('DOMContentLoaded', function () {
+    // Bar Chart
+    var barCtx = document.getElementById('barChart').getContext('2d');
+    var barChart = new Chart(barCtx, {
+        type: 'bar',
+        data: {
+            labels: [{% for category in categories %}"{{ category.Category }}", {% endfor %}],
+            datasets: [{
+                label: 'Total Quantity per Category',
+                data: [{% for category in categories %}{{ category.total_quantity }}, {% endfor %}],
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            },
+            responsive: true,
+            maintainAspectRatio: false, // Add this line to adjust aspect ratio
+        }
+    });
+
+    // Pie Chart (Brand)
+    var pieCtx = document.getElementById('pieChart').getContext('2d');
+    var pieChart = new Chart(pieCtx, {
+        type: 'pie',
+        data: {
+            labels: [{% for brand in brands %}"{{ brand.Brand }}", {% endfor %}],
+            datasets: [{
+                data: [{% for brand in brands %}{{ brand.total_quantity }}, {% endfor %}],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false, // Add this line to adjust aspect ratio
+        }
+    });
+});
